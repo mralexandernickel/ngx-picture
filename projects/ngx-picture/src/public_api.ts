44da8fd984d254ddf626ad37333ff7a9972f6818
Alexander Nickel <mr.alexander.nickel@gmail.com>
2019-01-10 09:53:24 +0100

    Add NgxPictureCacheService to public API

--- projects/ngx-picture/src/public_api.ts
+++ projects/ngx-picture/src/public_api.ts
@@ -1,5 +1,6 @@
 export * from './lib/typings';
 export * from './lib/ngx-picture.component';
+export * from './lib/ngx-picture-cache.service';
 export * from './lib/ngx-fallback-image.token';
 export * from './lib/ngx-picture.module';
 export * from './lib/images.mock.spec';
